806279c6b5e18f6d7bdd81ad46f0d50fbc60a539
vikas thakur <vikas098thakur@gmail.com>
2026-08-01 01:26:26 +0530

    Change axios base URL to production server

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL || "https://quickai-server-fp2s.onrender.com";
 
 export default axios;
